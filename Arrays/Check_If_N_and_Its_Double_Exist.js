cca0634e97183961aeb8336eacc5548592b338e4
Sumant Kataria <sumant.kataria@gmail.com>
2021-02-06 21:01:21 +0530

    Added file

---
+++ Arrays/Check_If_N_and_Its_Double_Exist.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function(arr) {
+    
+    // Check if N & its Double Exist
+    for(let i=0; i<arr.length; i++) {
+        for(let j=0; j<arr.length; j++) {
+            if(arr[i] === arr[j]*2 && i!== j) {
+                return true
+            }
+        }
+    }
+    return false
+};
