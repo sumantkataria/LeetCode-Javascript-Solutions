d6916f31ae93d95be967cfe2e85dc85f9c235695
Sumant Kataria <sumant.kataria@gmail.com>
2021-05-30 15:47:52 +0530

    added file

---
+++ Arrays and Strings/Reverse_String.js
@@ -0,0 +1,13 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+ var reverseString = function(s) {
+    if (s.length === 0) return "";
+    let start = 0, end = s.length - 1;
+    while(start < end) {
+      [s[start], s[end]] = [s[end], s[start]]
+      start++; end--;
+    }
+    return s
+};
