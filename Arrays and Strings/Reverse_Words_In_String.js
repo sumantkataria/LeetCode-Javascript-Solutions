59d406df65aa44f7ac3e4039515ff49dcb9fdc1b
Sumant Kataria <sumant.kataria@gmail.com>
2021-05-31 02:09:27 +0530

    added file

---
+++ Arrays and Strings/Reverse_Words_In_String.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var reverseWords = function(s) {
+    let arr = s.split(" ").filter(str => str.length !== 0)
+    return arr.reverse().join(" ")
+};
