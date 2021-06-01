5200f7bcc66908f8178b998062b26dd4f1d6147a
Sumant Kataria <sumant.kataria@gmail.com>
2021-06-02 01:01:51 +0530

    added file

---
+++ Linked List/Linked_List_Cycle_II.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var detectCycle = function(head) {
+    if(!head || !head.next) return null;
+    let slow = head, fast = head
+    while(fast !== null && fast.next !== null) {
+        slow = slow.next
+        fast = fast.next.next
+        if(slow === fast) break
+    }
+    if(fast === null || fast.next === null) return null
+    slow = head
+    while(fast !== slow) {
+        slow = slow.next
+        fast = fast.next
+    }
+    return slow
+};
