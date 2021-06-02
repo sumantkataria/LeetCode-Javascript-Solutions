b0fac3606052ace2ac454b67d0c59d0bf8c112ea
Sumant Kataria <sumant.kataria@gmail.com>
2021-06-02 13:22:30 +0530

    added file

---
+++ Linked List/Remove_Nth_Node_From_End_Of_List.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+ var removeNthFromEnd = function(head, n) {
+    let dummy = new ListNode(0);
+    dummy.next = head;
+    let first = second = dummy;
+    // Move first pointer so that the gap between first and second is n nodes apart
+    for (let i = 0; i <= n; i++) {
+        first = first.next;
+    }
+    // Move to the end, maintaining the gap
+    while (first != null) {
+        first = first.next;
+        second = second.next;
+    }
+    second.next = second.next.next;
+    return dummy.next;
+};
