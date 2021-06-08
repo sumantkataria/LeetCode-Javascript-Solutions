9bb5eaeb33482a99c5fb7cf8320a0cfe61d64742
Sumant Kataria <sumant.kataria@gmail.com>
2021-06-09 01:05:04 +0530

    added solution

---
+++ Binary Search/Find_Peak_Element.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var findPeakElement = function(nums) {
+    let start = 0, end = nums.length-1, mid
+    while(start < end) {
+      mid = ~~((start+end)/2)
+      if(nums[mid] > nums[mid+1]) {
+        end = mid
+      } else {
+        start = mid + 1
+      }
+    }
+    return start
+};
